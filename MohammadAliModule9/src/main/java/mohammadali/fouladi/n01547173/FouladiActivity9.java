b9f991d79117697b9e7bfbd9ef752d3d3f8cc680
origin <mh.fouladi@hotmail.com>
2024-07-16 17:25:21 -0400

    Added Menu

--- MohammadAliModule9/src/main/java/mohammadali/fouladi/n01547173/FouladiActivity9.java
+++ MohammadAliModule9/src/main/java/mohammadali/fouladi/n01547173/FouladiActivity9.java
@@ -1,24 +1,126 @@
 package mohammadali.fouladi.n01547173;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class FouladiActivity9 extends AppCompatActivity {
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class FouladiActivity9 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_MODE = "dark_mode";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Mo1e()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+        SharedPreferences sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean(PREF_DARK_MODE, false);
+
+        if (isDarkMode) {
+            setTheme(R.style.Theme_NavigationDrawerDark);
+
+        }
     }
+    private void searchGoogle(String searchPhrase) {
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.putExtra(SearchManager.QUERY, searchPhrase);
+        startActivity(intent);
+
+        // Hide keyboard
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_toggle_mode) {
+            // Handle toggle mode action
+            SharedPreferences sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+            boolean isDarkMode = sharedPreferences.getBoolean(PREF_DARK_MODE, false);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(PREF_DARK_MODE, !isDarkMode);
+            editor.apply();
+
+
+            return true;
+        } else if (id == R.id.action_search) {
+            // Handle search action
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.Search);
+            final EditText input = new EditText(this);
+            builder.setView(input);
+            builder.setPositiveButton(R.string.Search, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String searchPhrase = input.getText().toString().trim();
+                    if (!searchPhrase.isEmpty()) {
+                        searchGoogle(searchPhrase);
+                    }
+                }
+            });
+            builder.show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Mo1e()).commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+
 }
