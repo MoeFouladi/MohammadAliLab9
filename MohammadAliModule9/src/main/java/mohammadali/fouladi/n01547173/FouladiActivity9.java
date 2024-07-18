e64957a089a7e212bffc076d29ef764a9094ff93
origin <mh.fouladi@hotmail.com>
2024-07-18 00:50:19 -0400

    Added dark and light mode

--- MohammadAliModule9/src/main/java/mohammadali/fouladi/n01547173/FouladiActivity9.java
+++ MohammadAliModule9/src/main/java/mohammadali/fouladi/n01547173/FouladiActivity9.java
@@ -1,8 +1,11 @@
 package mohammadali.fouladi.n01547173;
 
+import static android.content.res.Configuration.UI_MODE_NIGHT_NO;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -13,6 +16,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,6 +35,9 @@ public class FouladiActivity9 extends AppCompatActivity implements NavigationVie
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean(PREF_DARK_MODE, false);
+        setTheme(isDarkMode ? R.style.Theme_NavigationDrawerDark : R.style.Theme_NavigationDrawer);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -50,13 +57,8 @@ public class FouladiActivity9 extends AppCompatActivity implements NavigationVie
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Mo1e()).commit();
             navigationView.setCheckedItem(R.id.Mo1e);
         }
-        SharedPreferences sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        boolean isDarkMode = sharedPreferences.getBoolean(PREF_DARK_MODE, false);
 
-        if (isDarkMode) {
-            setTheme(R.style.Theme_NavigationDrawerDark);
 
-        }
     }
     private void searchGoogle(String searchPhrase) {
         Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
@@ -82,16 +84,22 @@ public class FouladiActivity9 extends AppCompatActivity implements NavigationVie
 
         if (id == R.id.action_toggle_mode) {
             // Handle toggle mode action
-            SharedPreferences sharedPreferences = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+            SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
             boolean isDarkMode = sharedPreferences.getBoolean(PREF_DARK_MODE, false);
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putBoolean(PREF_DARK_MODE, !isDarkMode);
             editor.apply();
+            if (!isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);}
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);}
+
+
 
 
             return true;
         } else if (id == R.id.action_search) {
-            // Handle search action
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle(R.string.Search);
             final EditText input = new EditText(this);
@@ -114,11 +122,11 @@ public class FouladiActivity9 extends AppCompatActivity implements NavigationVie
     }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.Mo1e){
-                       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Mo1e()).commit();}
-                else if (item.getItemId() == R.id.Foula2i){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Foula2i()).commit();
-                }
+        if (item.getItemId() == R.id.Mo1e){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Mo1e()).commit();}
+        else if (item.getItemId() == R.id.Foula2i){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Foula2i()).commit();
+        }
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
